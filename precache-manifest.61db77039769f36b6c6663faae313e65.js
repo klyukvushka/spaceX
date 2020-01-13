3e7c343c20c19410bf608e2a7b9b82b1ddad92ac
klyukvushka <katyalex103@gmail.com>
2020-01-13 22:44:39 +0200

    Updates

---
+++ precache-manifest.61db77039769f36b6c6663faae313e65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec89931a0448bf406f9ec62dcee5bd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e759b474a6eb61f4e8",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d01719a01ea1b23f5f1d",
+    "url": "/static/css/main.fcb1a3ad.chunk.css"
+  },
+  {
+    "revision": "e8e759b474a6eb61f4e8",
+    "url": "/static/js/2.daa3be6a.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.daa3be6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d01719a01ea1b23f5f1d",
+    "url": "/static/js/main.1282138f.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
