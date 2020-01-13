a5918839f1daab30de818e50fd4a70a2b4ba0e55
klyukvushka <katyalex103@gmail.com>
2020-01-13 22:37:53 +0200

    sorting added

--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,69 @@ const instance = axios.create({
 
 export default class App extends Component {
   state = {
-    data: []
+    data: [],
+    currentPage: 0
   };
 
-  componentDidMount() {
-    axios.get(instance.defaults.baseURL).then(response => {
+  componentDidMount = () => {
+    instance.get().then(response => {
       const data = response.data;
       this.setState({ data });
     });
-  }
+  };
+
+  pageChangeHandler = ({ selected }) =>
+    this.setState({ currentPage: selected });
+
+  sortDesc = () => {
+    instance.get("?order=desc").then(response => {
+      const data = response.data;
+
+      this.setState({ data });
+    });
+  };
+
+  sortAsc = () => {
+    instance.get("?order=asc").then(response => {
+      const data = response.data;
+      this.setState({ data });
+    });
+  };
+
+  chunkArray = (arr, size) =>
+    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+      arr.slice(i * size, i * size + size)
+    );
 
   render() {
     const { data } = this.state;
+
+    const visibleData = this.chunkArray(data, 20);
     return (
       <main>
         <section className="launch">
           <div className="container">
             <h1 className="launch__title">SpaceX launches</h1>
+            <button onClick={this.sortDesc} className="launch__button">
+              Sort ↓
+            </button>
+            <button
+              onClick={this.sortAsc}
+              className="launch__button launch__button_asc"
+            >
+              Sort ↑
+            </button>
             <Table data={data} />
             <ReactPaginate
-              previousLabel={"Prev"}
-              nextLabel={"Next"}
+              previousLabel={"prev"}
+              nextLabel={"next"}
               breakLabel={"..."}
               breakClassName={"break-me"}
-              pageCount={this.state.pageCount}
+              pageCount={5}
               marginPagesDisplayed={2}
-              pageRangeDisplayed={4}
-              onPageChange={this.handlePageClick}
+              pageRangeDisplayed={5}
+              onPageChange={this.pageChangeHandler}
               containerClassName={"pagination"}
-              subContainerClassName={"pages pagination"}
               activeClassName={"active"}
               pageClassName="page-item"
               pageLinkClassName="page-link"
@@ -46,6 +80,7 @@ export default class App extends Component {
               nextClassName="page-item"
               previousLinkClassName="page-link"
               nextLinkClassName="page-link"
+              forcePage={this.state.currentPage}
             />
           </div>
         </section>
